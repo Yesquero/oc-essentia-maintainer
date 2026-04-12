0545132ed76f60030f215cd2b597edcccda0305b
Yesquero <yesquero1986@gmail.com>
2026-04-12 07:28:31 +0300

    Started Maintainer class implementation, basic testing setup

--- src/maintainer.lua
+++ src/maintainer.lua
@@ -1,7 +1,63 @@
+local serialization = require( "serialization" )
+
 local essentiaMaintainer = {}
 
-essentiaMaintainer.EssentiaMaintainer = {
-
+local config = {
+    defaultPriority = 0,
+    pollingInterval = 10,
+    recordsPath = "/etc/oces-records.txt"
 }
+
+essentiaMaintainer.Maintainer = {
+    aspectList = {},
+    defaultCfgPath = "str",
+    config = config
+}
+
+function essentiaMaintainer.Maintainer:new( o )
+    o = o or {}
+    setmetatable( o, self )
+    self.__index = self
+    return o
+end
+
+-- TODO: create file with default values if none exists
+function essentiaMaintainer.Maintainer:readConfig()
+    local file = assert( io.open( self.defaultCfgPath, "r" ), "Could not open config file." )
+    self.config = assert( serialization.unserialize( file:read( "a" ) ), "Error when reading config file." )
+    file:close()
+end
+
+-- TODO: handle empty file case
+function essentiaMaintainer.Maintainer:readRecords()
+    local file = assert( io.open( self.config.recordsPath, "r" ), "Could not open records file." )
+    self.aspectList = assert( serialization.unserialize( file:read( "a" ) ), "Error when reading records file." )
+    file:close()
+end
+
+function essentiaMaintainer.Maintainer:addAspect( name, amount, priority )
+    assert( name and type( name ) == "string" )
+    assert( amount and type( amount == "number" ) )
+
+    self:readRecords()
+    self.aspectList[#self.aspectList+1] = {
+        name = name,
+        amount = amount,
+        priority = priority or self.config.defaultPriority
+    }
+
+    local file = assert( io.open( self.config.recordsPath, "w" ) )
+    file = assert( io.open( self.config.recordsPath, "w" ), "Error when writing to records file." )
+    file:write( serialization.serialize( self.aspectList ) )
+    file:close()
+end
+
+function essentiaMaintainer.Maintainer:deleteAspect()
+    error( "Not implemented" )
+end
+
+function essentiaMaintainer.Maintainer:showAspectList()
+    error( "Not implemented" )
+end
 
 return essentiaMaintainer
--- test/test-main.lua
+++ test/test-main.lua
@@ -1,1 +1,50 @@
-print( "main tests" )
+local essentiaMaintainer = require( "maintainer" )
+
+local constants = {
+    cfgPath = "test/data/oces.cfg",
+    recordsPath = "test/data/oces-records.txt"
+}
+
+local function clearRecords()
+    local file = assert( io.open( constants.recordsPath, "w" ) )
+    file:write( "{}" )
+    file:close()
+end
+
+local function maintainerSetupTest()
+    local maintainer = essentiaMaintainer.Maintainer:new( { defaultCfgPath = constants.cfgPath } )
+
+    assert( maintainer.config.defaultPriority == 0 )
+    assert( maintainer.config.pollingInterval == 10 )
+    assert( maintainer.config.recordsPath == "/etc/oces-records.txt" )
+
+    maintainer:readConfig()
+    assert( maintainer.config.defaultPriority == -1 )
+    assert( maintainer.config.pollingInterval == 5 )
+    assert( maintainer.config.recordsPath == constants.recordsPath )
+
+    clearRecords()
+
+    maintainer:readRecords()
+    assert( #maintainer.aspectList == 0 )
+
+    maintainer:addAspect( "Vitium", 100, 25 )
+    maintainer:addAspect( "Metallum", 1000, nil )
+    assert( #maintainer.aspectList == 2 )
+
+    maintainer.aspectList = {}
+    assert( #maintainer.aspectList == 0 )
+
+    maintainer:readRecords()
+    assert( #maintainer.aspectList == 2 )
+    assert( maintainer.aspectList[1].name == "Vitium" )
+    assert( maintainer.aspectList[2].priority == maintainer.config.defaultPriority )
+end
+
+local function runTests()
+    maintainerSetupTest()
+
+    print( "Tests finished" )
+end
+
+runTests()
